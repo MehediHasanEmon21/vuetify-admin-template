aea4d4efab7c2f76780b9cc39823c1b9c4f4e1c0
MehediHasanEmon21 <mh.emon2021@gmail.com>
2022-06-28 20:08:14 +0600

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="inspire">
+    
+    <Sidebar :drawer="drawer"></Sidebar>
+    <Header @toggleDrawer="drawer = !drawer"></Header>
+
+    <v-main>
+      <!--  -->
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+import Sidebar from './components/inc/Sidebar.vue';
+import Header from './components/inc/Header.vue';
+  export default {
+    name: 'App',
+    data(){
+      return {
+        drawer: true,
+      }
+    },
+    components: {
+      Sidebar,
+      Header
+    }
   }
-}
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/inc/Sidebar.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+    <v-img 
+      src="https://cdn.pixabay.com/photo/2022/06/03/03/50/beach-7239311_960_720.jpg"
+      height="140"
+      class="pa-4"
+      >
+      <div class="text-center">
+          <v-avatar
+            class="mb-4"
+            color="grey darken-1"
+            size="80"
+          >
+          <v-img src="https://cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_960_720.png"></v-img>
+          </v-avatar>
+
+          <div class="white--text">john@vuetifyjs.com</div>
+       
+      </div>
+    </v-img>
+
+      <v-divider></v-divider>
+
+      <v-list>
+        <v-list-item
+          v-for="[icon, text] in links"
+          :key="icon"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ text }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+      <!--  -->
+    </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data(){
+    return {
+        links: [
+          ['mdi-inbox-arrow-down', 'Dashboard'],
+          ['mdi-send', 'Profile'],
+          ['mdi-delete', 'Product'],
+          ['mdi-alert-octagon', 'Orders'],
+        ],
+    }
+  },
+  props: ["drawer"],
+}
+</script>
+
+<style>
+
+</style>
